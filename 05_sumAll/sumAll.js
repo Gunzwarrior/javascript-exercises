f5aacbe9d7834bcc466d37b0739f4ff3f6746122
Thomas Rialland <rialland.thomas@hotmail.fr>
2022-01-06 19:56:58 +0100

    Clean syntax

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -2,7 +2,9 @@ const sumAll = function(firstNumber, lastNumber) {
   let sum = 0;
   let highNumber;
   let lowNumber;
-  if (firstNumber < 0 || lastNumber < 0 || !Number.isInteger(firstNumber) || !Number.isInteger(lastNumber)) {
+  if (firstNumber < 0 || lastNumber < 0
+    || !Number.isInteger(firstNumber)
+    || !Number.isInteger(lastNumber)) {
     return "ERROR";
   } else if (firstNumber > lastNumber) {
     highNumber = firstNumber;
