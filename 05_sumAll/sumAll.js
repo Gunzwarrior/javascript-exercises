5d9d6491663d039ba691f424a7d039080ba14580
Thomas Rialland <rialland.thomas@hotmail.fr>
2022-01-06 19:56:05 +0100

    Complete the exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -2,7 +2,7 @@ const sumAll = function(firstNumber, lastNumber) {
   let sum = 0;
   let highNumber;
   let lowNumber;
-  if (firstNumber < 0 || lastNumber < 0) {
+  if (firstNumber < 0 || lastNumber < 0 || !Number.isInteger(firstNumber) || !Number.isInteger(lastNumber)) {
     return "ERROR";
   } else if (firstNumber > lastNumber) {
     highNumber = firstNumber;
