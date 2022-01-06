f13d4a47266bf83c2258ecc5dc612a49f2aab2d3
Thomas Rialland <rialland.thomas@hotmail.fr>
2022-01-06 18:35:21 +0100

    Complete first test

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
 const reverseString = function(str) {
-
+  let strArray = [];
+  const strLength = str.length;
+  for (i = 0; i < strLength; i++) {
+    const currentChar = str.charAt(i);
+    strArray.push(currentChar);
+  }
+  const strArrayReversed = strArray.reverse();
+  const strReversed = strArrayReversed.join("");
+  return strReversed;
 };
 
 // Do not edit below this line
