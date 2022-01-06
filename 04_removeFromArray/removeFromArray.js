6c061886f877a53470fe39c9406f7b0ffa1d7b13
Thomas Rialland <rialland.thomas@hotmail.fr>
2022-01-06 19:19:54 +0100

    Complete third test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,7 +1,9 @@
 const removeFromArray = function(array, itemRemoved) {
   const argLength = arguments.length;
   for (i = 1; i <=argLength - 1; i++) {
-    array.splice(array.indexOf(arguments[i]), 1);
+    if (array.indexOf(arguments[i]) !== -1) {
+      array.splice(array.indexOf(arguments[i]), 1);
+    }
   }
   
   return array;
