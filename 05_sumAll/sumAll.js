38be4a99c2c09b6424be0bbdb883773dd4b0a938
Thomas Rialland <rialland.thomas@hotmail.fr>
2022-01-06 19:48:37 +0100

    Complete fourth step

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -2,7 +2,9 @@ const sumAll = function(firstNumber, lastNumber) {
   let sum = 0;
   let highNumber;
   let lowNumber;
-  if (firstNumber > lastNumber) {
+  if (firstNumber < 0 || lastNumber < 0) {
+    return "ERROR";
+  } else if (firstNumber > lastNumber) {
     highNumber = firstNumber;
     lowNumber = lastNumber;    
   } else {
