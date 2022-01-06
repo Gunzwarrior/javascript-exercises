a6a206c15059c56383084bcd76e318a088d9b56a
Thomas Rialland <rialland.thomas@hotmail.fr>
2022-01-06 18:41:50 +0100

    Clean the code with the solution

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,13 +1,5 @@
 const reverseString = function(str) {
-  let strArray = [];
-  const strLength = str.length;
-  for (i = 0; i < strLength; i++) {
-    const currentChar = str.charAt(i);
-    strArray.push(currentChar);
-  }
-  const strArrayReversed = strArray.reverse();
-  const strReversed = strArrayReversed.join("");
-  return strReversed;
+  return str.split("").reverse().join("");
 };
 
 // Do not edit below this line
