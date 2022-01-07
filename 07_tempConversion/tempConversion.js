fb5586baec35d952cbeeab516ff379c8d9a3cb3e
Thomas Rialland <rialland.thomas@hotmail.fr>
2022-01-07 18:33:22 +0100

    Complete step 4

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -2,8 +2,8 @@ const ftoc = function(temp) {
   return temp = Math.round(((temp - 32) / 1.8) * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  return temp = temp * 1.8 + 32;
 };
 
 // Do not edit below this line
