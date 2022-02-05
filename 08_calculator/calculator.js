0febbb4618ac86b9f598f36740d36a2ce97ffcc3
Thomas Rialland <rialland.thomas@hotmail.fr>
2022-02-06 00:50:52 +0100

    Complete the exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -10,16 +10,24 @@ const sum = function(array) {
 return array.reduce((total, item) => total + item, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+return array.reduce((total, item) => total * item);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+	let result = 1;
+  if (x === 0) {
+    return result;
+  } else {
+    for (let i = 1; i <= x; i++) {
+      result *= i;
+    };
+    return result;
+  }
 };
 
 // Do not edit below this line
