af18d53d22293a9603dc2845b068bc38e89183da
Thomas Rialland <rialland.thomas@hotmail.fr>
2022-02-06 14:36:15 +0100

    Complete exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,25 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+  let aAge;
+  let bAge;
+  let oldest = array.sort((a, b) => {
+    if (a.yearOfDeath) {
+      aAge = a.yearOfDeath - a.yearOfBirth;
+    } else {
+      aAge = (new Date().getFullYear()) - a.yearOfBirth;
+    };
+    if (b.yearOfDeath) {
+      bAge = b.yearOfDeath - b.yearOfBirth;
+    } else {
+      bAge = (new Date().getFullYear()) - b.yearOfBirth;
+    }
+    if (aAge < bAge) {
+      return 1;
+    } else {
+      return -1;
+    }
+  });
 
+  return oldest[0];
 };
 
 // Do not edit below this line
