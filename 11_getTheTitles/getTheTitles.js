84e2538a0ac2e63211126e479c8a769b9d59f007
Thomas Rialland <rialland.thomas@hotmail.fr>
2022-02-06 13:55:17 +0100

    Complete exercise

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+  let newArray = array.map((element, title) => element.title)
+  return newArray;
 };
 
 // Do not edit below this line
