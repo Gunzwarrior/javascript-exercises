74fe60b71f392228934778d812441a06af3c269d
Thomas Rialland <rialland.thomas@hotmail.fr>
2022-02-06 01:26:20 +0100

    Complete second test

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
-
+const palindromes = function (phrase) {
+  let arrayPhrase = phrase.split("");
+  let filteredArrayPhrase = arrayPhrase.filter((element) => {
+    if (element === "!" || element === "." || element === ","
+        || element === " ") {
+          return false;
+        } else {
+          return true;
+        }
+  });
+  let filteredPhrase = filteredArrayPhrase.join("");
+  let reversedArrayPhrase = filteredArrayPhrase.reverse();
+  let reversedPhrase = reversedArrayPhrase.join("");
+  return filteredPhrase === reversedPhrase ? true : false;
 };
 
 // Do not edit below this line
